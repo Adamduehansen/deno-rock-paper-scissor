e5978f2df81261d3dd0c5be9ecc93ce4db019dd1
Adam Due Hansen <adamduehan@gmail.com>
2022-01-26 17:03:49 +0100

    Moves router to index.routes.ts.

---
+++ index.router.ts
@@ -0,0 +1,34 @@
+import { Router, Body } from './deps.ts';
+import { play, getRandomPlayableHand } from './gameEngine.ts';
+
+const getValueFromBody = async function (body: Body) {
+  const value = await body.value;
+  const playedValue = value.get('text');
+  return playedValue;
+};
+
+const router = new Router();
+
+router.get('/', (context) => {
+  context.response.body = 'Rock, papper, scissor API.';
+});
+
+router.post('/', async (context) => {
+  const playedValue = await getValueFromBody(context.request.body());
+  const computerHand = getRandomPlayableHand();
+
+  try {
+    const result = await play(playedValue, computerHand);
+    if (result.win) {
+      context.response.body = `Computer played ${result.computerHand}, you win! 🥳`;
+    } else if (result.win === undefined) {
+      context.response.body = `Computer played ${result.computerHand}, its a tie! 🥸`;
+    } else {
+      context.response.body = `Computer played ${result.computerHand}, you loose! 😭`;
+    }
+  } catch (error) {
+    context.response.body = error.message;
+  }
+});
+
+export default router;
--- mod.ts
+++ mod.ts
@@ -1,42 +1,9 @@
-import {
-  Application,
-  Router,
-  Body,
-} from 'https://deno.land/x/oak@v10.1.0/mod.ts';
+import { Application } from './deps.ts';
 import * as log from 'https://deno.land/std@0.122.0/log/mod.ts';
-import { play, getRandomPlayableHand } from './gameEngine.ts';
-
-const getValueFromBody = async function (body: Body) {
-  const value = await body.value;
-  const playedValue = value.get('text');
-  return playedValue;
-};
+import router from './index.router.ts';
 
 const port = Number(Deno.env.get('PORT')) || 8080;
 const application = new Application();
-const router = new Router();
-
-router.get('/', (context) => {
-  context.response.body = 'Rock, papper, scissor API.';
-});
-
-router.post('/', async (context) => {
-  const playedValue = await getValueFromBody(context.request.body());
-  const computerHand = getRandomPlayableHand();
-
-  try {
-    const result = await play(playedValue, computerHand);
-    if (result.win) {
-      context.response.body = `Computer played ${result.computerHand}, you win! 🥳`;
-    } else if (result.win === undefined) {
-      context.response.body = `Computer played ${result.computerHand}, its a tie! 🥸`;
-    } else {
-      context.response.body = `Computer played ${result.computerHand}, you loose! 😭`;
-    }
-  } catch (error) {
-    context.response.body = error.message;
-  }
-});
 
 application.use(router.routes());
 
