da24d931b0a0c5e652996af729c707ac6983c2f6
Adam Due Hansen <adamduehan@gmail.com>
2022-01-27 21:31:58 +0100

    Adds loading of environment variables.

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
+import 'https://deno.land/x/dotenv/load.ts';
 import { log } from './deps.ts';
 import application from './server.ts';
 
-const port = Number(Deno.env.get('PORT')) || 8080;
+const port = Number(Deno.env.get('PORT'));
 
 if (import.meta.main) {
   log.info(`Starting rock paper scissor server on port ${port}...`);
