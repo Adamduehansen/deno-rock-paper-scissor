89be0e81cdf870de47d2696b61079c6ee32d0100
Adam Due Hansen <adamduehan@gmail.com>
2022-01-28 00:07:52 +0100

    Results are now stored in database.

--- middleware/storeGameResultMiddleware.ts
+++ middleware/storeGameResultMiddleware.ts
@@ -1,5 +1,23 @@
+import * as postgres from 'https://deno.land/x/postgres@v0.14.2/mod.ts';
+import 'https://deno.land/x/dotenv/load.ts';
 import { Context } from '../deps.ts';
 import { ApplicationState, getValueFromBody } from '../server.ts';
+
+const databaseUrl = Deno.env.get('DATABASE_URL')!;
+const pool = new postgres.Pool(databaseUrl, 3, true);
+
+const connection = await pool.connect();
+try {
+  await connection.queryObject`
+    CREATE TABLE IF NOT EXISTS game_result (
+      id SERIAL PRIMARY KEY,
+      user_id TEXT NOT NULL,
+      win BOOLEAN
+    )
+  `;
+} finally {
+  connection.release();
+}
 
 const storeGameResultMiddleware = async function (
   context: Context<ApplicationState, ApplicationState>,
@@ -8,7 +26,10 @@ const storeGameResultMiddleware = async function (
   await next();
   const { gameResult } = context.state;
   const userId = await getValueFromBody(context.request.body(), 'user_id');
-  console.log(`Player id is ${userId}`);
+  await connection.queryObject`
+    INSERT INTO game_result (user_id, win) 
+    VALUES (${userId}, ${gameResult.win})
+  `;
 };
 
 export default storeGameResultMiddleware;
